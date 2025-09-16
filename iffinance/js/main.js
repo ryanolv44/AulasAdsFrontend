6cd4c28a61f3e6d0c3a37e990cc441d117e7b590
Ryan Oliveira <o.ryan@aluno.ifsp.edu.br>
2025-09-16 20:06:46 -0300

    updateEdit

--- iffinance/js/main.js
+++ iffinance/js/main.js
@@ -1,24 +1,24 @@
-function openModal(modalId){
-    const modal = document.querySelector(modalId)
-    modal.style.display = "flex"
+function openModal(modalId) {
+  const modal = document.querySelector(modalId);
+  modal.style.display = "flex";
 }
 
-function closeModal(modalId){
-    const modal = document.querySelector(modalId)
-    modal.style.display = "none"
+function closeModal(modalId) {
+  const modal = document.querySelector(modalId);
+  modal.style.display = "none";
 }
 
-function addTicker(event){
-    event.preventDefault()
+function addTicker(event) {
+  event.preventDefault();
 
-    const ticker = event.target.ticker.value
-    const bolsa = event.target.bolsa.value
-    const valor = event.target.valor.value
-    const ativos = event.target.ativos.value
+  const ticker = event.target.ticker.value;
+  const bolsa = event.target.bolsa.value;
+  const valor = event.target.valor.value;
+  const ativos = event.target.ativos.value;
 
-    const total = valor * ativos
+  const total = valor * ativos;
 
-    const card = `
+  const card = `
         <div class="card-stock" id="${ticker}" onmouseenter="showButtons(event)" onmouseleave="hideButtons(event)">
             <header>
                 <h2>${ticker}</h2>
@@ -29,82 +29,95 @@ function addTicker(event){
             </main>
             <footer>
                 <span>Ativos: <span>${ativos}</span></span>
-                <span>US$ ${total}</span>
+                <span class="total">US$ <span>${total}</span></span>
             </footer>
             <div class="buttons">
                 <button type="button" onclick="openEditCard(event)">Editar</button>
                 <button type="button" onclick="deleteCard(event)">Excluir</button>
             </div>
         </div>
-    `
-    const cards = document.querySelector("#cards")
-    cards.innerHTML += card 
-    closeModal('#add')
+    `;
+  const cards = document.querySelector("#cards");
+  cards.innerHTML += card;
+
+  event.target.reset();
+
+  closeModal("#add");
 }
 
-function editTicker(event){
-    event.preventDefault()
+function editTicker(event) {
+  event.preventDefault();
 
-    const idcard = event.target.idcard.value
-    const ticker = event.target.editticker.value
-    const bolsa = event.target.editbolsa.value
-    const valor = event.target.editvalor.value
-    const ativos = event.target.editativos.value
+  const idcard = event.target.idcard.value;
+  const ticker = event.target.editticker.value;
+  const bolsa = event.target.editbolsa.value;
+  const valor = event.target.editvalor.value;
+  const ativos = event.target.editativos.value;
 
-    const total = valor * ativos
+  const total = valor * ativos;
 
-    const cardStockEdit = document.getElementById(idcard)
-    console.log(cardStockEdit)
+  const cardStockEdit = document.getElementById(idcard);
 
-    const h2ticker = cardStockEdit.querySelector('header h2')
-    h2ticker.innerText = ticker
- 
-    //Todo
-    
-    closeModal('#edit')
+  const h2Ticker = cardStockEdit.querySelector("header h2");
+  h2Ticker.innerText = ticker;
+
+  const h3Bolsa = cardStockEdit.querySelector("header h3");
+  h3Bolsa.innerText = bolsa;
+
+  const spanValor = cardStockEdit.querySelector("main span span");
+  spanValor.innerText = valor;
+
+  const spanAtivos = cardStockEdit.querySelector("footer span span");
+  spanAtivos.innerText = ativos;
+
+  const spanTotal = cardStockEdit.querySelector("footer span.total span");
+  spanTotal.innerText = total;
+
+  closeModal("#edit");
 }
 
-function showButtons(event){
-    const cardStock = event.target
-    const buttons = cardStock.querySelector(".buttons")
-    buttons.style.display = "flex"
+function showButtons(event) {
+  const cardStock = event.target;
+  const buttons = cardStock.querySelector(".buttons");
+  buttons.style.display = "flex";
 }
 
-function hideButtons(event){
-    const cardStock = event.target
-    const buttons = cardStock.querySelector(".buttons")
-    buttons.style.display = "none"
+function hideButtons(event) {
+  const cardStock = event.target;
+  const buttons = cardStock.querySelector(".buttons");
+  buttons.style.display = "none";
 }
 
-function deleteCard(event){
-    const buttonDelete = event.target
-    const cardStock = buttonDelete.closest(".card-stock")
-    cardStock.remove()
+function deleteCard(event) {
+  const buttonDelete = event.target;
+  const cardStock = buttonDelete.closest(".card-stock");
+  cardStock.remove();
 }
 
-function openEditCard(event){
-    const buttonEdit = event.target
-    const cardStock = buttonEdit.closest(".card-stock")
+function openEditCard(event) {
+  const buttonEdit = event.target;
+  const cardStock = buttonEdit.closest(".card-stock");
 
-    const ticker = cardStock.querySelector('header h2').innerText
-    const inputEditTicker = document.getElementById('editticker')
-    inputEditTicker.value = ticker
+  const ticker = cardStock.querySelector("header h2").innerText;
+  const inputEditTicker = document.getElementById("editticker");
+  inputEditTicker.value = ticker;
 
-    const inputIdCard = document.getElementById('idcard')
-    inputIdCard.value = ticker
+  const idCard = cardStock.getAttribute("id");
+  //cardStock.setAttribute("teste", "123") //Exemplo de setAttribute para adicionar um atributo em um elemento
+  const inputIdCard = document.getElementById("idcard");
+  inputIdCard.value = idCard;
 
-    const bolsa = cardStock.querySelector('header h3').innerText
-    const selectEditBolsa = document.getElementById('editbolsa')
-    const option = selectEditBolsa.querySelector(`option[value=${bolsa}]`)
-    option.setAttribute('selected', 'true')
+  const bolsa = cardStock.querySelector("header h3").innerText;
+  const selectEditBolsa = document.getElementById("editbolsa");
+  selectEditBolsa.value = bolsa;
 
-    const valor = cardStock.querySelector('main span span').innerText
-    const inputEditValor = document.getElementById('editvalor')
-    inputEditValor.value = valor
+  const valor = cardStock.querySelector("main span span").innerText;
+  const inputEditValor = document.getElementById("editvalor");
+  inputEditValor.value = valor;
 
-    const ativos = cardStock.querySelector('footer span span').innerText
-    const inputEditAtivos = document.getElementById('editativos')
-    inputEditAtivos.value = ativos
+  const ativos = cardStock.querySelector("footer span span").innerText;
+  const inputEditAtivos = document.getElementById("editativos");
+  inputEditAtivos.value = ativos;
 
-    openModal('#edit')
+  openModal("#edit");
 }
